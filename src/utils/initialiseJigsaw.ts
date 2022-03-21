1ce66e468b38ed90e4099f9ac522cfd2107f62c1
Calvin Cheng <calvin.cc.cheng@gmail.com>
2022-03-21 15:35:31 +0800

    Shuffle and spread pieces on initialisation

--- src/utils/initialiseJigsaw.ts
+++ src/utils/initialiseJigsaw.ts
@@ -4,18 +4,31 @@ import generateJigsaw from "utils/generateJigsaw";
 import { YPiece } from "types";
 import { WORLD_WIDTH, WORLD_HEIGHT } from "constants";
 
-function initialiseJigsaw(n: number, m: number, size: number) {
+export default function initialiseJigsaw(n: number, m: number, size: number) {
   // Generate edges
   const edges = generateJigsaw(n, m);
 
-  // Add new pieces to yPieces
+  // Get a list of shuffled indices to randomise piece locations
+  const locations = [];
+  for (let i = 0; i < m; i += 1) {
+    for (let j = 0; j < n; j += 1) {
+      locations.push([i, j]);
+    }
+  }
+  const shuffledLocations = shuffle(locations);
+
+  // Populate yPieces
+  const gap = 2 * size;
   const newPieces: YPiece[] = [];
   edges.forEach((row: any, i: number) => {
     row.forEach((edge: any, j: number) => {
       const newPiece: YPiece = new Y.Map();
-      const offsetX = (WORLD_WIDTH - n * size) / 2;
-      const offsetY = (WORLD_HEIGHT - m * size) / 2;
-      newPiece.set("pos", [offsetX + j * size, offsetY + i * size]);
+
+      const [I, J] = shuffledLocations.pop();
+      const offsetX = WORLD_WIDTH / 2 - (n * gap) / 2;
+      const offsetY = WORLD_HEIGHT / 2 - (m * gap) / 2;
+      newPiece.set("pos", [offsetX + J * gap, offsetY + I * gap]);
+
       newPiece.set("index", [i, j]);
       newPiece.set("size", size);
       newPiece.set("edges", edge);
@@ -30,4 +43,25 @@ function initialiseJigsaw(n: number, m: number, size: number) {
   });
 }
 
-export default initialiseJigsaw;
+/**
+ * Helper function - Fisher-Yates shuffle
+ */
+function shuffle(array: any[]): any[] {
+  let currentIndex = array.length;
+  let randomIndex = null;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
