2c09df834c1d46fd26fd451eb82211a7b3dd0dbf
Calvin Cheng <calvin.cc.cheng@gmail.com>
2022-03-21 14:30:20 +0800

    Remove mask spritesheet

--- src/contexts/jigsaw.tsx
+++ src/contexts/jigsaw.tsx
@@ -2,10 +2,7 @@ import { useState, useEffect, useContext, createContext } from "react";
 import * as PIXI from "pixi.js";
 import imagePath from "../assets/greatWave.png";
 // import imagePath from "../assets/greatWaveSmall.png";
-// import maskPath from "../assets/masksheet_tiny.png";
-// import maskPath from "../assets/masksheet_small.png";
-// import maskPath from "../assets/masksheet.png";
-import maskPath from "../assets/masksheet_nostroke.png";
+// import imagePath from "../assets/monalisa.webp";
 import { useApp as usePixi } from "@inlet/react-pixi";
 import { YPiece } from "../types";
 
@@ -29,11 +26,10 @@ export const JigsawProvider = ({ pieces, children }: JigsawProviderProps) => {
   useEffect(() => {
     pixi.loader.reset();
     pixi.loader.add("jigsaw", imagePath);
-    pixi.loader.add("mask", maskPath);
 
     pixi.loader.load(() => {
       const newBaseTextures: BaseTextures = {};
-      ["jigsaw", "mask"].forEach((textureName) => {
+      ["jigsaw"].forEach((textureName) => {
         const resource = pixi.loader.resources[textureName];
         newBaseTextures[textureName] = PIXI.BaseTexture.from(resource.url);
       });
