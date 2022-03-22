66d7d775023b59b061729581c3f6877ae75019b5
Calvin Cheng <calvin.cc.cheng@gmail.com>
2022-03-22 16:20:32 +0800

    Memoise cursor emotion styling to prevent repeated creation of <style> nodes in <head>

--- src/components/Cursor/index.tsx
+++ src/components/Cursor/index.tsx
@@ -16,8 +16,17 @@ type CursorProps = {
   interpolate?: boolean;
 };
 
+const cursorStyle = css`
+  top: 0px;
+  left: 0px;
+  position: absolute;
+  height: ${CURSOR_SIZE}px;
+  width: ${CURSOR_SIZE}px;
+  pointer-events: none;
+`;
+
 const Cursor = ({
-  pos,
+  pos: [x, y],
   name,
   color = "var(--color-white)",
   chat = "",
@@ -26,23 +35,15 @@ const Cursor = ({
   active = true,
   interpolate = false,
 }: CursorProps) => {
-  const [x, y] = pos;
   const cursorColor = active ? color : "var(--color-black4)";
-
   return (
     <div
-      css={css`
-        top: ${y}px;
-        left: ${x}px;
-        position: absolute;
-        height: ${CURSOR_SIZE}px;
-        width: ${CURSOR_SIZE}px;
-        pointer-events: none;
-        transition: ${interpolate ? "0.1s ease all" : "none"};
-      `}
+      css={cursorStyle}
+      style={{
+        transform: `translate(${x}px, ${y}px)`,
+      }}
     >
       <CursorIcon fill={cursorColor} />
-
       {(chatting || name) && (
         <ChatBubble
           name={name}
