36a1be0d65077a5a0649fa3922de785bbf5c8684
Calvin Cheng <calvin.cc.cheng@gmail.com>
2022-03-23 20:29:21 +0800

    Add /jigly/ base

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    port: 3000,
-  },
-});
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      port: 3000,
+    },
+    base: "/jigly/",
+  });
