9b81dbd8d834c796849d0bf6a31b33462461c4e6
Calvin Cheng <calvin.cc.cheng@gmail.com>
2022-03-23 20:30:08 +0800

    Use yjs-websocket demo server on non-dev environments

--- src/Y.ts
+++ src/Y.ts
@@ -3,13 +3,13 @@ import { WebsocketProvider } from "y-websocket";
 
 const doc = new Y.Doc();
 
-const provider = new WebsocketProvider(
-  "ws://localhost:1234",
-  // "ws://192.168.68.120:1234",
-  `jigly-demo`,
-  doc,
-  { connect: true }
-);
+const endpoint = import.meta.env?.DEV
+  ? "ws://localhost:1234"
+  : "wss://demos.yjs.dev";
+
+const provider = new WebsocketProvider(endpoint, `jigly-demo`, doc, {
+  connect: true,
+});
 const awareness = provider.awareness;
 
 export { doc, provider, awareness };
